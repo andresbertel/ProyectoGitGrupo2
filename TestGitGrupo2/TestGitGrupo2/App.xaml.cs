8d1bba0a1a2700930ab791019bf3447b3af42c19
DESKTOP-4FVJ3U1\Ing Andres Bertel <andres.bertel@hotmail.com>
2024-10-11 08:26:10 -0500

    Se ajusta proyecto base

---
+++ TestGitGrupo2/TestGitGrupo2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TestGitGrupo2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TestGitGrupo2/TestGitGrupo2/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TestGitGrupo2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
